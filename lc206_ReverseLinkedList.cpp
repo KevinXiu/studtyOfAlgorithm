0443042e925f67d47a8e742c58e822611fcd6bfa
KevinXiu <1475787640@qq.com>
2020-03-09 22:18:14 +0800

    LeetCode206

---
+++ lc206_ReverseLinkedList.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+struct ListNode
+{
+	int val;
+	ListNode *next;
+};
+
+ListNode *reserseList(ListNode *head)
+{
+	ListNode *p = head;
+	ListNode *p1,*new_head = NULL;
+	while(p)
+	{
+		p1=p->next;
+		p->next=new_head;
+		new_head=p;
+		p=p1;	
+	}
+	return new_head;
+}
+int main()
+{
+	return 0;
+ } 
